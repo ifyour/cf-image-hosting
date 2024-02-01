54fcd587ad0883825627bbcaf6bd29c934fdada7
ifyour <ifyour@outlook.com>
2024-02-01 23:49:06 +0800

    Update dev and deploy scripts and API_HOST variable

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/cloudflare-workers";
 
-const host = `https://telegra.ph`;
-const app = new Hono();
+const app = new Hono<{ Bindings: { API_HOST: string } }>();
 
-app.get("/", serveStatic({ root: "./public" }));
+app.get("/*", serveStatic({ root: "./public" }));
 
 app.post("/upload", async (c) => {
   const body = await c.req.parseBody();
   const formData = new FormData();
   // Telegraph ignores filenames, so we can use any filename we want!
   formData.append("file", body.file as Blob, "test.png");
-  return fetch(`${host}/upload`, {
+  return fetch(`${c.env.API_HOST}/upload`, {
     method: "POST",
     body: formData,
   });
 });
 
 app.get("/file/:fileName", async (c) => {
-  return fetch(`${host}/file/${c.req.param("fileName")}`);
+  return fetch(`${c.env.API_HOST}/file/${c.req.param("fileName")}`);
 });
 
 export default app;
