7c2accb0f156bad4dc81ea0423d2b84ee75da6d1
ifyour <ifyour@outlook.com>
2024-02-04 00:39:56 +0800

    Update style and layout

--- src/public/script.js
+++ src/public/script.js
@@ -1,9 +1,9 @@
 const fileInput = document.getElementById("fileInput");
 const uploadStatus = document.getElementById("uploadStatus");
-const container = document.querySelector(".container");
+const fullWindow = document.querySelector(".full-window");
 
 document.addEventListener("paste", onFilePaste);
-container.addEventListener("drop", onFileDrop);
+fullWindow.addEventListener("drop", onFileDrop);
 fileInput.addEventListener("change", onFileChange);
 
 function onFileChange() {
@@ -47,41 +47,47 @@ function onFileUrlCopy() {
   const imageUrl = document.getElementById("imageUrl");
   imageUrl.select();
   document.execCommand("copy");
-  document.querySelector(".copy-btn").textContent = "✨ Copied";
+  document.querySelector(".copy-btn").textContent = "Copied ✨";
   setTimeout(() => {
-    document.querySelector(".copy-btn").textContent = "Copy URL";
+    document.querySelector(".copy-btn").textContent = "Copy";
   }, 1000);
 }
 
 function handleUpload(file) {
   const formData = new FormData();
   formData.append("file", file);
-  document.querySelector(".upload-btn").textContent = "Uploading...";
+  document.querySelector(".upload-text").textContent = "Uploading...";
+  document.querySelector(".spinner-grow").classList.remove("d-none");
   fetch("/upload", { method: "POST", body: formData })
     .then((response) => response.json())
     .then((data) => {
+      if (data && data.error) {
+        throw new Error(data.error);
+      }
       const src = window.location.origin + data[0].src;
       uploadStatus.innerHTML = `
-                <div class="alert alert-success">Upload successful! 🥳</div>
-                <div class="input-group">
-                  <input type="text" class="form-control" id="imageUrl" value="${src}">
-                  <div class="input-group-append">
-                    <button class="btn btn-outline-secondary copy-btn" type="button">Copy URL</button>
-                  </div>
-                </div>
-                <br />
-                <img src="${src}" class="img-fluid mb-3" alt="Uploaded Image">
-            `;
-
+      <div class="alert alert-success text-center">Successful 🥳</div>
+      <div class="input-group">
+        <input type="text" class="form-control" id="imageUrl" value="${src}">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary copy-btn" type="button">Copy</button>
+        </div>
+      </div>
+      <img style="margin-top: 10px" src="${src}" class="img-fluid mb-3" alt="Uploaded Image">
+      `;
       document
         .querySelector(".copy-btn")
         .addEventListener("click", onFileUrlCopy);
     })
-    .catch(() => {
-      uploadStatus.innerHTML =
-        '<div class="alert alert-danger">Upload failed. Please try again.</div>';
+    .catch((error) => {
+      uploadStatus.innerHTML = `
+      <div class="alert alert-danger">${
+        error || "Upload failed. Please try again."
+      }</div>
+      `;
     })
     .finally(() => {
-      document.querySelector(".upload-btn").textContent = "Upload an Image";
+      document.querySelector(".upload-text").textContent = "Upload Again";
+      document.querySelector(".spinner-grow").classList.add("d-none");
     });
 }
