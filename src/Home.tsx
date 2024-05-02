78273f242c48a27dc6cf3e644625231e33f2b354
ifyour <ifyour@outlook.com>
2024-05-02 21:28:00 +0800

    fix: Update viewport meta

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,10 @@ export function Home() {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
         <title>Telegraph Image Hosting</title>
         <link
           rel="stylesheet"
